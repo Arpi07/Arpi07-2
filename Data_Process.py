ca8655297fac6d93e04ee89d6efe86321d000b32
Arpi07 <50438477+Arpi07@users.noreply.github.com>
2020-11-04 00:42:45 +0530

    Update Data_Process.py

--- Data_Process.py
+++ Data_Process.py
@@ -9,16 +9,16 @@ from scipy.io import loadmat
 
 import Attention_cnn.datasets_utils
 import Attention_cnn.train.training_utils
-from Attention_cnn import Dataset
+from Attention_cnn import Data
 
 
 
 def matlab_mat_to_numpy():
     """Conversion of colour infoormation to an nd array"""
-    colors = loadmat(Dataset.COLORMAP_ORIG_PATH)['colors']
+    colors = loadmat(Data.COLORMAP_ORIG_PATH)['colors']
     background_colour = np.zeros([1, 3], dtype=np.uint8)
     colors = np.concatenate([background_colour, colors], axis=0)
-    np.save(Dataset.COLORMAP_PATH[:-4], colors)
+    np.save(Data.COLORMAP_PATH[:-4], colors)
 
 
 def parse_object_info():
@@ -26,7 +26,7 @@ def parse_object_info():
     is_header = True
     # integer_id 
     meta_data = {}
-    with open(Dataset.ORIG_OBJECT_INFO_PATH, 'r') as text_file:
+    with open(Data.ORIG_OBJECT_INFO_PATH, 'r') as text_file:
         for row in text_file:
             if is_header:
                 is_header = False
@@ -43,7 +43,7 @@ def parse_object_info():
                     'train': train,
                     'val': val,
                     'names': names,}
-    with open(Dataset.OBJECT_INFO_PATH, 'wb') as pfile:
+    with open(Data.OBJECT_INFO_PATH, 'wb') as pfile:
         pickle.dump(meta_data, pfile)
 
 
@@ -55,7 +55,7 @@ def parse_object_info():
 def edge_path_from_label_path(label_path):
     label_name = os.path.basename(label_path)
     label_dir = os.path.dirname(label_path)
-    edge_name = Dataset.EDGE_PREFIX + label_name
+    edge_name = Data.EDGE_PREFIX + label_name
     edge_path = os.path.join(label_dir, edge_name)
     return edge_path
 
@@ -63,16 +63,16 @@ def edge_path_from_label_path(label_path):
 def label_path_to_edge_saved(label_path):
     edge_path = edge_path_from_label_path(label_path)
     label = imageio.imread(label_path)
-    edge = Attention_cnn.train.training_utils.flat_label_to_edge_label(label, Dataset.N_CLASSES)
+    edge = Attention_cnn.train.training_utils.flat_label_to_edge_label(label, Data.N_CLASSES)
     imageio.imsave(edge_path, edge)
 
 #labelling of boundary edge mask
 def create_edge_labels():
     pool = multiprocessing.Pool(10)
-    train_labels = [os.path.join(Dataset.TRAINING_ANNOTATION_DIR, x) for x in os.listdir(
-        Dataset.TRAINING_ANNOTATION_DIR)]
-    val_labels = [os.path.join(Dataset.VALIDATION_ANNOTATION_DIR, x) for x in os.listdir(
-        Dataset.VALIDATION_ANNOTATION_DIR)]
+    train_labels = [os.path.join(Data.TRAINING_ANNOTATION_DIR, x) for x in os.listdir(
+        Data.TRAINING_ANNOTATION_DIR)]
+    val_labels = [os.path.join(Data.VALIDATION_ANNOTATION_DIR, x) for x in os.listdir(
+        Data.VALIDATION_ANNOTATION_DIR)]
 
     num_train = len(train_labels)
     print('creating training edge maps')
@@ -94,7 +94,7 @@ def get_dataset():
     # Labelling of boundary edge mask
     create_edge_labels()
     # Dataset dictionary
-   Attention_cnn.datasets_utils.list_files(Dataset.DATA_DIR)
+   Attention_cnn.datasets_utils.list_files(Data.DATA_DIR)
 
 
 if __name__ == '__main__':
