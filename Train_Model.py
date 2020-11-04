46cad45b63749e765af808e8e255fb0f3aa7b939
Arpi07 <50438477+Arpi07@users.noreply.github.com>
2020-11-04 14:54:35 +0530

    Update and rename scene_parsing_model.py to Train_Model.py

--- Train_Model.py
+++ Train_Model.py
@@ -1,41 +1,38 @@
 import tensorflow as tf
 
-from gated_shape_cnn import datasets
-import gated_shape_cnn.datasets.scene_parsing_data.dataset
+from Attention_cnn import Data
+import Attention_cnn.Data.Get_Dataset
 import os
 
 os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 
-from gated_shape_cnn import GSCNN
-from gated_shape_cnn import Trainer
+from Attention_cnn import ACNN
+from Attention_cnn import Trainer
 
 
-# define dataset parameters
+#dataset parameters
 batch_size = 8
 network_input_h = network_input_w = 256
 max_crop_downsample = 0.95
 colour_aug_factor = 0.2
 
 # loss hyperparams
-l1 = 1.
-l2 = 20.
-l3 = 1.
-l4 = 1.
-loss_weights = [l1, l2, l3, l4]
+l1 = 17.
+l2 = 1.
+
+loss_weights = [l1, l2]
 
 # optimiser
-n_train_images = 20210
+n_train_images = 1000
 n_steps_in_epoch = n_train_images // batch_size
 momentum = 0.9
 learning_rate_fn = tf.keras.optimizers.schedules.PolynomialDecay(
-    1e-2,
-    n_steps_in_epoch * 230,
+    0.001,
+    n_steps_in_epoch ,
     0.000001)
-optimiser = tf.keras.optimizers.SGD(learning_rate=learning_rate_fn, momentum=momentum)
-# optimiser = tf.keras.optimizers.RMSprop()
-
-# build the dataset loader
-dataset_loader = gated_shape_cnn.datasets.scene_parsing_data.dataset.SceneParsing(
+optimiser = tf.keras.optimizers.adam(learning_rate=learning_rate_fn, epsilon=1e-05)
+#Dataset Loader....
+dataset_loader = Attention_cnn.Data.Get_Dataset.SceneParsing(
     batch_size,
     network_input_h,
     network_input_w,
@@ -44,18 +41,18 @@ dataset_loader = gated_shape_cnn.datasets.scene_parsing_data.dataset.SceneParsin
     build_for_keras=False,
     debug=False)
 
-# build the model
-model = GSCNN(n_classes=gated_shape_cnn.datasets.scene_parsing_data.N_CLASSES)
+# Model building..................
+model = ACNN(n_classes=Attention_cnn.Data.N_CLASSES)
 
 # train
 trainer = Trainer(
     model,
     dataset_loader.build_training_dataset(),
     dataset_loader.build_validation_dataset(),
-    epochs=300,
+    epochs=2000,
     optimiser=optimiser,
-    log_dir='logsScene',
-    model_dir='logsScene/model',
+    log_dir='logsBalloon',
+    model_dir='logsBalloon/model',
     loss_weights=loss_weights,
     accumulation_iterations=2,)
 trainer.train_loop()
